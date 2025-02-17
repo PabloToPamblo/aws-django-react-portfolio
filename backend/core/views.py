f7887c39cedb1f7eced4f81322c461b1a323d8ba
PabloToPamblo <PabloToPamblo@gmail.com>
2025-02-17 22:09:48 +0700

    JWT

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,23 +1,33 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.models import User
+from .models import ContactMessage
 # from django.shortcuts import render
 
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def home(request):
     return Response({"message": "Bienvenido a la API de AWS-Django-React"})
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def contact(request):
     name = request.data.get('name')
     email = request.data.get('email')
     message = request.data.get('message')
-    return Response({"message": f"Gracias {name}, tu mensaje ha sido recibido."})
+
+    if name and email and message:
+        ContactMessage.objects.create(name=name, email=email, message=message)
+        return Response({"message": "Mensaje guardado correctamente."})
+    return Response({"error": "Todos los campos son obligatorios."}, status=400)
+
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def register(request):
     username = request.data.get('username')
     email = request.data.get('email')
